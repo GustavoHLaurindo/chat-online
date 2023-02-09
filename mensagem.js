129aa2aabace692e6484134ae353d06dcf86bb46
GustavoHLaurindo <97362581+GustavoHLaurindo@users.noreply.github.com>
2023-02-09 17:02:59 -0300

    termino da parte de design começo do javascript

---
+++ mensagem.js
@@ -0,0 +1,4 @@
+chat = document.getElementById("chat")
+mensagem_purple = document.getElementsByClassName("circle-i")
+mensagem_blue = document.getElementsByClassName("circle-you")
+mensagem_purple.add
